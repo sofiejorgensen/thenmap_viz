a64db04d29ce5d47acfa014563b632b139a13934
sofiejorgensen <sofficial.jorgensen@gmail.com>
2020-10-02 15:47:34 +0200

    improved Shiny app

--- thenmap_viz/app.R
+++ thenmap_viz/app.R
@@ -1,73 +1,35 @@
-# # install.packages("httr")
-# library(httr)
-# 
-# # install.packages("jsonlite")
-# library(jsonlite)
-# 
-# #install.packages("geojsonR")
-# library(geojsonR)
-# 
-# 
-# # install.packages("RCurl")
-# library(RCurl)
-
-# install.packages("geojsonio")
-# library(geojsonio)
-
 devtools::install_github("hankolofs/api_fetch")
 library(apiFetch)
-
 library(sp)
-
 library(shiny)
-
-# Extra plot
 library(ggplot2)
 library(dplyr)
-require(maps)
-require(viridis)
-
-theme_set(
-  theme_void()
-)
 
 
-# Define UI for application
+
+# Define UI for thenmap_viz application
 ui <- fluidPage(
   # App Title
-  titlePanel("The thenmap_vize App"),
-  titlePanel("Hello Shiny Shiny World!"),
+  titlePanel("The thenmap_viz App"),
   
-  # Sidebar layout with input and output definitions ----
+  # Sidebar layout 
   sidebarLayout(
     
-    # Sidebar panel for inputs ----
+    # Sidebar panel 
     sidebarPanel(
-      h1("First level title"),
-      h4(code("This is going to be amazing!")),
-      br(), # New line
-      h6("a paragraph of text."),
-      p("p creates a paragraph of text."),
-      # Widgets
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "year",
-                  label = "Select year:",
-                  min = 1900,
-                  max = 2020,
-                  value = 30),
+      h1("Visualize historical and current borders"),
+      h4(code("Select a date to create a map!")),
       
-      fluidRow(column(3, selectInput("select", h3("Country"), 
-                                     choices = list("All countries" = 1, "Sweden" = 2,
-                                                    "Hello land" = 3), selected = 1)),
-               column(3, dateInput(inputId = "date",
+      # Input: date
+      fluidRow(column(4, dateInput(inputId = "date",
                                    h3("Date"),
-                                   value = "2000-01-01"))
-      )
+                                   value = "2020-01-01"))
+      ),
     ),
     
-    # Main panel for displaying outputs ----
+    # Main panel for displaying a map
     mainPanel(
-      # Output: Map ----
+      # Output: Map
       plotOutput(outputId = "distPlot"),
       textOutput(outputId = "selected_var")
       
@@ -76,26 +38,19 @@ ui <- fluidPage(
   
 )
 
-# Define server logic ----
+# Define server 
 server <- function(input, output) {
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
+  # National borders of Thenmap data
   output$distPlot <- renderPlot({
-    # Standard plot
     map_data_year <- fetch(input$date)$data
     map_bg <- fetch("2020-01-01")$data
-    ggplot() + geom_sf(data = map_bg, fill = "grey", color = "grey") + geom_sf(data = map_data_year, fill = "#638f54", color = "white")
+    ggplot() + 
+      geom_sf(data = map_bg, fill = "grey", color = "grey") +
+      geom_sf(data = map_data_year, fill = "#638f54", color = "white")
 
   })
   output$selected_var <- renderText(
-    paste0("Well hello there, you have selected the following date: ", input$date)
+    paste0("The green area corresponds to the national borders on ", input$date, ".")
   )
 }
 
